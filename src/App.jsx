a102f84596670f82b5cd0f3215dc31dc22e589a0
mikhailkolesnikov <mikhail.kolesnikov@outlook.de>
2024-12-26 16:44:33 +0100

    homework

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@
 
 //импорт CSS
 import './App.css'
- //import WelcomeCard from './Homeworks/WelcomeCard'
-import Lesson02 from './lessons/lesson02/Lesson02'
+ import WelcomeCard from './Homeworks/WelcomeCard'
+// import Lesson02 from './lessons/lesson02/Lesson02'
 
 
 function App() {
@@ -12,8 +12,8 @@ function App() {
 
   return (
     <>
-      <Lesson02/>
-      {/* <WelcomeCard /> */}
+      {/* <Lesson02/> */}
+      <WelcomeCard />
     </>
   )
 }
